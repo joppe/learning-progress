6c44308ec48e300ed40fe36999f5ee8ca1b240f0
Joppe Aarts <joppe@apestaartje.info>
2021-08-18 17:31:31 +0200

    show the goals on the homepage by retrieving them server side

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export async function load({ fetch }) {
+    const url = '/goal/list';
+    const res = await fetch(url);
+
+    return {
+      props: {
+        goals: await res.json()
+      }
+    };
+  }
+</script>
+
+<script>
+  export let goals;
+</script>
+
+<h1>Learning progress</h1>
+
+<ul>
+  {#each goals as goal}
+    <li>{JSON.stringify(goal)}</li>
+  {/each}
+</ul>
+
