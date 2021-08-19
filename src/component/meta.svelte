2c9a34051cfef7989896da1e3f9e663b3b0fdda6
Joppe Aarts <joppe@apestaartje.info>
2021-08-19 18:08:14 +0200

    show current part in meta component

---
+++ src/component/meta.svelte
@@ -0,0 +1,27 @@
+<script type="ts">
+  import type { Goal } from '$lib/entity/goal/goal';
+
+  export let goal: Goal
+</script>
+
+<style>
+  dl {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+  }
+
+  dt {
+    font-weight: bold;
+  }
+</style>
+
+<dl>
+  <dt>Topic</dt>
+  <dd>{goal.topic}</dd>
+  <dt>Parts</dt>
+  <dd>{goal.parts.type}</dd>
+  <dt>Count</dt>
+  <dd>{goal.parts.count}</dd>
+  <dt>Current</dt>
+  <dd>{goal.progress.current}</dd>
+</dl>
