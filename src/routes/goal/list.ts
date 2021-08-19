7d434534f2ccb91feff5c9d7cdfb2058f11069c0
Joppe Aarts <joppe@apestaartje.info>
2021-08-19 16:55:55 +0200

    added typings to goal list endpoint

--- src/routes/goal/list.ts
+++ src/routes/goal/list.ts
@@ -1,12 +1,11 @@
+import { connect } from '$lib/database/connect';
+import type { Goal } from '$lib/entity/goal/goal';
 import { Model } from '$lib/entity/goal/model';
-import { dbUrl } from '$lib/variables';
-import { connect } from 'mongoose';
+import type { EndpointOutput } from '@sveltejs/kit';
 
-export async function get() {
-  await connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+export async function get(): Promise<EndpointOutput<Goal[]>> {
+  await connect();
+
   const query = Model.find({});
   const list = await query.exec();
 
