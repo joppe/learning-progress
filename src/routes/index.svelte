d0e260067cb0a31eb641673c87a1a02ab9d74e29
Joppe Aarts <joppe@apestaartje.info>
2021-08-19 16:56:24 +0200

    render Goal component on homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,15 +11,20 @@
   }
 </script>
 
-<script>
-  export let goals;
+<script type="ts">
+  import type { Goal } from '$lib/entity/goal/goal';
+  import GoalComponent from '$component/goal.svelte';
+
+  export let goals: Goal[];
 </script>
 
 <h1>Learning progress</h1>
 
 <ul>
   {#each goals as goal}
-    <li>{JSON.stringify(goal)}</li>
+    <li>
+      <GoalComponent goal={goal} />
+    </li>
   {/each}
 </ul>
 
